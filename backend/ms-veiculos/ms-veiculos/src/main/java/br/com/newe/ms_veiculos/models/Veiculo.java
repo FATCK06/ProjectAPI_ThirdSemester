f4889c6058aaace5ef4d35a83ca5cc435e02e752
IgorMartins0729 <igor.mrt02@gmail.com>
2026-09-09 09:49:57 -0300

    chore (#3): Iniciando a pasta backend

---
+++ backend/ms-veiculos/ms-veiculos/src/main/java/br/com/newe/ms_veiculos/models/Veiculo.java
@@ -0,0 +1,10 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "veiculos")
+public class Veiculo {
+    @Id
+    private
+}
---
+++ backend/ms-veiculos/ms-veiculos/src/test/java/br/com/newe/ms_veiculos/MsVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.newe.ms_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
